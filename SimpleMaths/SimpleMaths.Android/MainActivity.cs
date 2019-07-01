73d718761e6a4bb6dbeb055bba98e0d4b199601a
mohanad oweidat <mohanad.oweidat@hotmail.com>
2019-07-01 20:25:59 +0700

    add-Icon

--- SimpleMaths/SimpleMaths.Android/MainActivity.cs
+++ SimpleMaths/SimpleMaths.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using CarouselView.FormsPlugin.Android;
 
 namespace SimpleMaths.Droid
 {
-    [Activity(Label = "SimpleMaths", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, 
+    [Activity(Label = "SimpleMaths", Icon = "@mipmap/Icons", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, 
         ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
